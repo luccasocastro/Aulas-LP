c907eb0a9af09db6035a69a9a986c7bd10e856a7
Luccas Castro <emaildocastro.adm01@gmail.com>
2021-11-07 01:15:48 -0300

    play with c++

--- C++/HelloWorld/main.cpp
+++ C++/HelloWorld/main.cpp
@@ -2,9 +2,32 @@
 
 using namespace std;
 
-int main(){
-    
-    cout << "Olá, Mundo!";
+class OlaMundo{
+    private:
+    string frase;
 
+    public:
+
+    void sayHello(){
+        cout << "Hello, "<< getPhrase() << endl;
+    }
+
+    string getPhrase(){
+        return frase;
+    }
+
+    void setPhrase(string ffrase){
+        frase = ffrase;
+    }
+};
+
+int main(){
+    OlaMundo ola;
+    string oi;
+    cout << "Informe um valor: " << endl;
+    cin >> oi;
+    ola.setPhrase(oi);
+    ola.sayHello();
+    
     return 0;
 }
